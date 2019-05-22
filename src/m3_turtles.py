66e91debf685dedeb5452a266c0134a1153ee50b
ivescai <caij1@rose-hulman.edu>
2019-05-22 19:06:58 -0400

    m3 done

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -131,7 +131,7 @@ turtleA.draw_regular_polygon(10,120)
 
 ########################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
