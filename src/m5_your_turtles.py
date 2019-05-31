3a903531d89b85c5379beff51e7d5d518091b016
ivescai <caij1@rose-hulman.edu>
2019-05-31 12:56:28 +0800

    m5 done？

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -5,8 +5,8 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
          their colleagues and PUT_YOUR_NAME_HERE.
 """
 ########################################################################
-# TODO: 1.
-# On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
+# DONE: 1.
+# On Line 5 above, replace  Junfei Cai  with your own name.
 ########################################################################
 
 ########################################################################
@@ -28,3 +28,48 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+turtleB = rg.SimpleTurtle('turtle')
+turtleB.pen = rg.Pen('Black', 3)
+turtleB.speed = 20
+
+size = 100
+
+for k in range(9):
+    turtleB.draw_regular_polygon(8, size)
+
+    turtleB.pen_up()
+    turtleB.right(90+22.5)
+    turtleB.forward(10)
+    turtleB.left(90+22.5)
+
+    turtleB.pen_down()
+    size = size + 10
+
+
+turtleR = rg.SimpleTurtle('turtle')
+turtleR.pen = rg.Pen('Red', 3)
+turtleR.speed = 10
+
+size = 120
+
+turtleR.pen_up()
+turtleR.forward(50)
+turtleR.pen_down()
+
+for k in range(8):
+    turtleR.draw_circle(size)
+
+    turtleR.pen_up()
+    turtleR.left(90)
+    turtleR.forward(10)
+    turtleR.right(90)
+
+    turtleR.pen_down()
+    size = size - 10
+
+window.close_on_mouse_click()
